609da20a4911a0f1c389bc7a7520799fa3e29385
alexeykosh <alexeykochevoy@gmail.com>
2022-09-20 16:18:26 +0200

    added the combination script

---
+++ combine_texts.py
@@ -0,0 +1,33 @@
+import pyconll
+import sys
+from os import (walk, 
+                remove)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3: #{
+        print('combine_texts.py <directory> <out>');
+        sys.exit(-1);
+    # }
+
+    directory = sys.argv[1]
+    out = sys.argv[2]
+
+    def get_filenames(directory):
+        files = []
+        for (dirpath, dirnames, filenames) in walk(directory):
+            files.extend([i for i in filenames if '.conllu' in i])
+            break
+        return sorted(files)
+    fn = get_filenames(directory)
+    with open(directory + '/train.conllu', 'w') as outfile:
+        for fname in fn:
+            with open(directory + '/' + fname) as infile:
+                outfile.write(infile.read())
+    train = pyconll.load_from_file(directory + '/train.conllu')
+    i = 1
+    for sentence in train:
+        sentence.id = i
+        i += 1
+    with open('train.conllu', 'w', encoding='utf-8') as f:
+        train.write(f)
+    remove(directory + '/train.conllu')
