8c73731fda3667037f68206c24d4e64a82c93b33
alexeykosh <alexeykochevoy@gmail.com>
2022-09-20 16:25:57 +0200

    small tweaks and a progress message is added

--- combine_texts.py
+++ combine_texts.py
@@ -3,21 +3,21 @@ import sys
 from os import (walk, 
                 remove)
 
+def get_filenames(directory):
+    files = []
+    for (dirpath, dirnames, filenames) in walk(directory):
+        files.extend([i for i in filenames if '.conllu' in i])
+        break
+    return sorted(files)
+
 if __name__ == '__main__':
-    if len(sys.argv) != 3: #{
-        print('combine_texts.py <directory> <out>');
-        sys.exit(-1);
-    # }
+    if len(sys.argv) != 3:
+        print('combine_texts.py <directory> <out>')
+        sys.exit(-1)
 
     directory = sys.argv[1]
     out = sys.argv[2]
 
-    def get_filenames(directory):
-        files = []
-        for (dirpath, dirnames, filenames) in walk(directory):
-            files.extend([i for i in filenames if '.conllu' in i])
-            break
-        return sorted(files)
     fn = get_filenames(directory)
     with open(directory + '/train.conllu', 'w') as outfile:
         for fname in fn:
@@ -30,4 +30,5 @@ if __name__ == '__main__':
         i += 1
     with open('train.conllu', 'w', encoding='utf-8') as f:
         train.write(f)
+    print(f'Combination is completed: {out} has {i} sentences.')
     remove(directory + '/train.conllu')
